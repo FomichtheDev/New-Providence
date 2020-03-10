e5abdd845ca2a4f2c2d53300d2646a1673e91f09
FomichtheDev <fomichg@gmail.com>
2020-03-10 19:40:25 +0200

    final version 0.1

--- js/main.js
+++ js/main.js
@@ -1,39 +1,77 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
+    $('.owl-carousel').owlCarousel({
+        nav: true,
+        navText: ['', ''],
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
 
-    // Scroll Events
-    $(window).scroll(function () {
-
-        var wScroll = $(this).scrollTop();
-
-        // Activate menu
-        if (wScroll > 20) {
-            $('#main-nav').addClass('active');
-            $('#slide_out_menu').addClass('scrolled');
-        } else {
-            $('#main-nav').removeClass('active');
-            $('#slide_out_menu').removeClass('scrolled');
-        }
-        ;
-
-
-        //Scroll Effects
-
-    });
-
-
-    // Navigation
-    $('#navigation').on('click', function (e) {
-        e.preventDefault();
-        $(this).addClass('open');
-        $('#slide_out_menu').toggleClass('open');
-
-        if ($('#slide_out_menu').hasClass('open')) {
-            $('.menu-close').on('click', function (e) {
-                e.preventDefault();
-                $('#slide_out_menu').removeClass('open');
-            })
+            1000: {
+                items: 1
+            }
         }
     });
 
+
+    var individual_price_table = $('.switcher').find('.starter');
+    var company_price_table = $('.switcher').find('.pro');
+
+
+    $('.switch-toggles').find('.individual').addClass('active');
+    $('.switcher').find('.starter').addClass('active');
+
+    $('.switch-toggles').find('.individual').on('click', function () {
+        $(this).addClass('active');
+        $('.switch-toggles').removeClass('active');
+        $(this).siblings().removeClass('active');
+        individual_price_table.addClass('active');
+        company_price_table.removeClass('active');
+    });
+
+    $('.switch-toggles').find('.company').on('click', function () {
+        $(this).addClass('active');
+        $('.switch-toggles').addClass('active');
+        $(this).siblings().removeClass('active');
+        company_price_table.addClass('active');
+        individual_price_table.removeClass('active');
+    });
+
+
+    $(window).on("scroll", function () {
+        if ($(window).scrollTop() > 50) $('header').addClass('sticky');
+        else $('header').removeClass('sticky');
+    });
+
+
+    wow = new WOW(
+        {
+            boxClass: 'wow',      // default
+            animateClass: 'animated', // default
+            offset: 0,          // default
+            mobile: true,       // default
+            live: true        // default
+        }
+    );
+    wow.init();
+
+    $(document).ready(function () {
+        $("nav").on("click", "a", function (event) {
+            event.preventDefault();
+            var id = $(this).attr('href'),
+                top = $(id).offset().top - 180;
+            $('body,html').animate({scrollTop: top}, 1500);
+        });
+    });
+
+
+    $('.popup-youtube').magnificPopup({
+        type: 'iframe'
+    });
 });
